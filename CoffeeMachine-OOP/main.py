90702c0576925cd15e00f206debf1d2f6caac652
yoyomonkey <yoyomonkey1000@hotmail.com>
2023-10-24 02:03:41 +0100

    Added off functionality

--- CoffeeMachine-OOP/main.py
+++ CoffeeMachine-OOP/main.py
@@ -3,26 +3,32 @@ import menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-jazz_coffee_menu = menu.Menu()
-jazz_coffee_machine = CoffeeMaker()
-jazz_money_machine = MoneyMachine()
 
-machine_operational = True
- 
-while machine_operational :
-    drink_choice =input(f"What would you like? {jazz_coffee_menu.get_items()}")
-    if drink_choice == "report":
-        jazz_coffee_machine.report()
-    else:
-        drink_selected = jazz_coffee_menu.find_drink(drink_choice)
-        if jazz_coffee_machine.is_resource_sufficient(drink_selected):
-            jazz_coffee_machine.make_coffee(drink_selected)
-            jazz_money_machine.make_payment(drink_selected.cost)
+def main_coffee():
+    jazz_coffee_menu = menu.Menu()
+    jazz_coffee_machine = CoffeeMaker()
+    jazz_money_machine = MoneyMachine()
+
+    machine_operational = True
+
+    while machine_operational :
+        drink_choice =input(f"What would you like? {jazz_coffee_menu.get_items()}")
+        if drink_choice == "off":
+            machine_operational = False
+        elif drink_choice == "report":
+            jazz_coffee_machine.report()
             jazz_money_machine.report()
         else:
-            machine_operational = False
+            drink_selected = jazz_coffee_menu.find_drink(drink_choice)
+            if jazz_coffee_machine.is_resource_sufficient(drink_selected):
+                jazz_money_machine.make_payment(drink_selected.cost)
+                jazz_coffee_machine.make_coffee(drink_selected)
+                jazz_money_machine.report()
+            else:
+                machine_operational = False
 
 
+main_coffee()
 
 
 
